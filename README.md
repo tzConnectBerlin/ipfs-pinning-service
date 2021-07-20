55bcf24fe7010fb3914f1cb33de202f9f458ae69
shekhar-shubhendu <shubh.shekhar1991@gmail.com>
2021-07-20 15:02:52 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# IPFS Pinning Service
+
+## Prerequisite
+
+- Node.js
+- Yarn
+
+## Environment File
+
+Create a `.env` file using `.env.example` and set your Pinata API key and Pinata Secret Key.
+
+## Available Scripts
+
+**Do not forget to do yarn install**
+
+- `yarn start` to start the service
+- `yarn debug` yo start the service in debug mode
+
+## Available Endpoints
+
+- POST `/add-json` to add and pin JSON.
---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+const pinataSDK = require("@pinata/sdk");
+const pinata = pinataSDK(
+  process.env.PINATA_API_KEY,
+  process.env.PINATA_API_SECRET
+);
+
+router.post("/add-json", async (req, res) => {
+  const result = await pinata.pinJSONToIPFS(req.body, {
+    pinataMetadata: {
+      name: req?.body?.name,
+    },
+  });
+  res.json(result);
+});
+
+module.exports = router;
